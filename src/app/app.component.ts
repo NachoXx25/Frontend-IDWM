3290d513f7b4c60c3ec2618fc90720496901ecef
NachoXx25 <105187185+NachoXx25@users.noreply.github.com>
2024-06-08 19:09:42 -0400

    chore(deps): Add Flowbite.js and Tailwind CSS for UI styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontendIDWM';
+
+
+  constructor() {
+    console.log('Hola desde el constructor');
+  }
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
