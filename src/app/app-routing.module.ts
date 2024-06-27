8b033628c2c1fb8b412e1e1fdbbf2d2b65beb9b5
NachoXx25 <105187185+NachoXx25@users.noreply.github.com>
2024-06-27 16:10:06 -0400

    feat: add the customer module and the corresponding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
   { path: '', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+
+
 ];
 
 @NgModule({
